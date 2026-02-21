d53db8ca7cf68b42a3af04f244cf90216d065c12
Makarios Faiz <110292021+makariosfaiz123@users.noreply.github.com>
2026-02-21 20:30:24 +0200

    Update src/Store/index.js
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/Store/index.js
+++ src/Store/index.js
@@ -10,7 +10,7 @@ const useMacBookStore = create((set)=>({
     textrue : '/videos/feature-1.mp4',
     setTextrue: (textrue)=>set({textrue}),
 
-    reset: ()=>set({color:'2e2c2e',scale : 0.08 , textrue : '/videos/feature-1.mp4'}),
+    reset: ()=>set({color:'#2e2c2e',scale : 0.08 , textrue : '/videos/feature-1.mp4'}),
 }));
 
 export default useMacBookStore ;
